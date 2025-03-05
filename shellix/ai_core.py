9776119988a74e50866a73c0cb83cc140054658d
Aydar Akhmetzyanov <aydar.tech@gmail.com>
2025-03-04 22:28:24 -0800

    tool improvements

--- shellix/ai_core.py
+++ shellix/ai_core.py
@@ -1,14 +1,11 @@
-from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
-from langgraph.prebuilt import create_react_agent
-from langgraph.prebuilt.chat_agent_executor import AgentState
-from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
-from langchain_community.tools import ShellTool
+from langgraph.prebuilt import create_react_agent
+
+from shellix.shell_tool import ShellTool
 
 shell_tool = ShellTool()
 
-#
 # @tool
 # def magic_function(input: int) -> int:
 #     """Applies a magic function to an input."""
@@ -21,15 +18,14 @@ prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a helpful console assistant called Shellix. "
                    "Your output and tool call results will be outputted to user terminal."
-                   "You can creatively use the terminal commands and tools provided to you to accomplish your tasks. For example ShellTool with 'cat' to read files."),
+                   "You can creatively use the terminal commands and tools provided to you to accomplish your tasks. For example ShellTool with 'cat' to read files. "
+                   "Feel free to traverse the current folder with 'ls' to accomplish your tasks."),
         ("placeholder", "{messages}"),
     ]
 )
 
 
-
 def process_input(input_str, credentials, current_directory):
-
     model = ChatOpenAI(model=credentials['OPENAI_MODEL'], temperature=0, api_key=credentials['OPENAI_KEY'])
     langgraph_agent_executor = create_react_agent(model, tools, prompt=prompt)
 
