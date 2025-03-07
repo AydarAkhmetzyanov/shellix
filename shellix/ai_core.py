f793cbd4d1db55d7cd72ea21c6472775379ead0f
Aydar Akhmetzyanov <aydar.tech@gmail.com>
2025-03-06 22:51:16 -0800

    Update ai_core.py

--- shellix/ai_core.py
+++ shellix/ai_core.py
@@ -12,7 +12,7 @@ import os
 
 def load_tools(credentials):
     search_tool = TavilySearchResults(
-        max_results=10,
+        max_results=5,
         search_depth="advanced",
         include_answer=True,
         include_raw_content=False,
