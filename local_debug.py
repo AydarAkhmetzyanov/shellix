5a3e782d282ff72893f137a843005c37cbf346fb
Aydar Akhmetzyanov <aydar.tech@gmail.com>
2025-03-08 00:14:49 -0800

    Update local_debug.py

--- local_debug.py
+++ local_debug.py
@@ -1,8 +1,10 @@
+import sys
 from shellix.ai_core import process_input
 
+COMMAND = "Run pwd and ls commands"
 
 def main():
-    user_input = "Run pwd and ls commands"
+    user_input = sys.argv[1] if len(sys.argv) > 1 else COMMAND
 
     print("\nProcessing...\n")
     process_input(user_input)
